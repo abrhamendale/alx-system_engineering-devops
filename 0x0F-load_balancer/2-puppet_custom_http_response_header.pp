aa1939a3c7417e6f2d2eca5c28e98d20e5a360d2
Abrham Endale <tmailae2020@gmail.com>
2023-01-18 17:27:21 -0800

    Added create server with puppet.

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,7 +3,7 @@
 exec { 'http header':
   command  => 'sudo apt-get update;
 	sudo apt-get install nginx;
-	sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
+	sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
 	sudo service nginx restart',
   provider => shell,
 }
