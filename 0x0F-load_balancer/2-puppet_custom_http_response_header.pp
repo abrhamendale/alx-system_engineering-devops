1653d1c7f1611f5e642025634e74a882f4a5966d
Abrham Endale <tmailae2020@gmail.com>
2023-01-19 18:40:23 -0800

    Fixed more errors.

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 #Create server using puppet
-exec {'sudo apt-get update':}
-exec {'sudo apt-get install nginx':}
-exec {'sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default':}
-exec { 'sudo service nginx restart':}
+exec { 'http header':
+  command  => 'sudo apt-get update -y;
+  	sudo apt-get install nginx -y;
+	sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
+	sudo service nginx restart',
+  provider => shell,}
